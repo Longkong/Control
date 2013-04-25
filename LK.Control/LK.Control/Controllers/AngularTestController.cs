68c707be2ef9f838a11a72e27aec64e9a7095a6a
teeraboon <teeraboon@longkongstudio.com>
2013-04-25 15:16:32 +0700

    start Custom UI Control

---
+++ LK.Control/LK.Control/Controllers/AngularTestController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LK.Control.Controllers
+{
+    public class AngularTestController : Controller
+    {
+        //
+        // GET: /AngularTest/
+
+        public ActionResult Run()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ LK.Control/LK.Control/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LK.Control.WebApiApplication" Language="C#" %>
