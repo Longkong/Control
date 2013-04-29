582d70dc9c671c6edacb1e9c9976c38debe725ec
teeraboon <teeraboon@longkongstudio.com>
2013-04-29 14:45:22 +0700

    prepare create directive

---
+++ LK.Control/LK.Control/Scripts/lk/directives/angular-lk.js
@@ -0,0 +1,39 @@
+﻿function Ctrl3($scope) {
+    $scope.title = 'Lorem Ipsum';
+    $scope.text = 'Neque porro quisquam est qui dolorem ipsum quia dolor...';
+}
+
+angular.module('zippyModule', [])
+  .directive('zippy', function () {
+      return {
+          restrict: 'C',
+          // This HTML will replace the zippy directive.
+          replace: true,
+          transclude: true,
+          scope: { title: '@zippyTitle' },
+          template: '<div>' +
+                      '<div class="title">{{title}}</div>' +
+                      '<div class="body" ng-transclude></div>' +
+                    '</div>',
+          // The linking function will add behavior to the template
+          link: function (scope, element, attrs) {
+              // Title element
+              var title = angular.element(element.children()[0]),
+                  // Opened / closed state
+                  opened = true;
+
+              // Clicking on title should open/close the zippy
+              title.bind('click', toggle);
+
+              // Toggle the closed/opened state
+              function toggle() {
+                  opened = !opened;
+                  element.removeClass(opened ? 'closed' : 'opened');
+                  element.addClass(opened ? 'opened' : 'closed');
+              }
+
+              // initialize the zippy
+              toggle();
+          }
+      }
+  });
