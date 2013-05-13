f106ca2fd06ad8dbf37986e71d55dd60ea1a196c
teeraboon <teeraboon@longkongstudio.com>
2013-05-13 17:38:45 +0700

    ทดลอง select2

---
+++ LK.Control/LK.Control/Controllers/Select2Controller.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LK.Control.Controllers
+{
+    public class Select2Controller : Controller
+    {
+        //
+        // GET: /Select2/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ LK.Control/LK.Control/Scripts/lk/Controller/Select2App.js
@@ -0,0 +1,42 @@
+﻿/// <reference path="../../angular.js" />
+var app = angular.module('myApp', ['ui']);
+
+app.controller('MainCtrl', function ($scope, $http) {
+    var items;
+
+    $http({ method: 'GET', url: '/api/master1', params: { req: '' } })
+                                 .success(function (data, status) {
+                                     $scope.master1s = data;
+                                 });
+
+    // Built-in support for ajax
+    $scope.version4 = {
+        query: function (query) {
+            var data = { results: [] };
+            //for (i = 1; i < 5; i++) {
+            //    s = "";
+            //    for (j = 0; j < i; j++) { s = s + query.term; }
+            //    data.results.push({ id: query.term + i, text: s });
+            //}
+            angular.forEach($scope.master1s, function (master) {
+                    data.results.push({ id: master.Id , text: master.Name ,obj:master});
+            });
+            query.callback(data);
+        }
+    }
+
+
+    $scope.addmore = function () {
+        console.log($scope.version4model);
+    }
+
+    $scope.version4model = "";
+
+    $scope.options = {
+        tags: ['red', 'yellow', 'blue']
+    };
+
+    $scope.selectedOptions = ['yellow'];
+});
+
+
