31e539920ad5667d56f71736904669ea280550f9
Jirapas <jirapas@longkongstudio.com>
2013-05-16 14:24:59 +0700

    timeframe directive

--- LK.Control/LK.Control/Scripts/lk/directives/angular-lk-timeframe.js
+++ LK.Control/LK.Control/Scripts/lk/directives/angular-lk-timeframe.js
@@ -1,7 +1,7 @@
 ﻿/// <reference path="../../angular.js" />
 
 //var tpltokenCompile;
-var module=angular.module('lk.timeframe', ['lk.timeframe.tokenFilters']);
+var module = angular.module('lk.timeframe', ['lk.timeframe.tokenFilters']);
 var buttonId, html, title;
 
 angular.module('lk.timeframe.tokenFilters', []).directive("lkTimeframefilters", function ($compile, $document, $parse, $http) {
@@ -10,16 +10,14 @@ angular.module('lk.timeframe.tokenFilters', []).directive("lkTimeframefilters", 
         requrie: 'ngModel',
         link: function (scope, element, attrs, ngModelCtrl) {
 
-            scope.showhide = "  Show Date line  ";
-
             scope.Filters = [];
             var Filtername = [];
             var Filtersum = [];
             var Filtercount = 0;
             var datenow = new Date();
             var datestring = datenow.getDate() + '/' + datenow.getMonth() + '/' + datenow.getFullYear();
-            //$http({ method: 'GET', url: '/search/CreateSearch' }).success(function (data, status) {
-           
+     
+
             var getter = $parse(attrs.lkTimeframefilters), setter = getter.assign, value = getter(scope), options = {};
 
             $http({ method: 'GET', url: value }).success(function (data, status) {
@@ -44,12 +42,12 @@ angular.module('lk.timeframe.tokenFilters', []).directive("lkTimeframefilters", 
                     }
 
                 }
-                scope.countline = "( "+Filtercount+" )";
+                scope.countline = "( " + Filtercount + " )";
                 scope.Filters.push(Filtersum[0]);
             });
-        
+
             scope.Adddropdowns = Filtername;
-            //console.log(scope.Adddropdowns);
+       
             scope.addDate = function (tf, index) {
                 this.Filters[index].typedate = tf;
                 if (tf == "Period") {
@@ -76,7 +74,7 @@ angular.module('lk.timeframe.tokenFilters', []).directive("lkTimeframefilters", 
 
             //Select Add
             scope.addContact = function (indexline, selected, name) {
-                console.log(selected);
+   
                 if (selected) {
                     this.Filters.push(Filtersum[indexline]);
                 } else {
@@ -87,27 +85,18 @@ angular.module('lk.timeframe.tokenFilters', []).directive("lkTimeframefilters", 
                     this.Filters.splice(indextimeframe, 1);
                 }
             };
-            //Show Line
-            scope.showline = function (showline) {
-           
-                if (showline) {
-                    this.showhide = "  Hide Date line  ";
-                } else {
-                    this.showhide = "  Show Date line  ";
-                }
-            };
+         
 
-          
             $(document).on('click', '.multiple-select-wrapper .list', function (e) {
-       
+
                 e.stopPropagation();
             });
-   
+
 
             $(document).on('click', '.selected-items-box', function (e) {
- 
+
                 e.stopPropagation();
-       
+
                 $('.multiple-select-wrapper .list').toggle('slideDown');
 
             });
@@ -117,8 +106,9 @@ angular.module('lk.timeframe.tokenFilters', []).directive("lkTimeframefilters", 
                 $('.multiple-select-wrapper .list').slideUp();
             });
             scope.chk = false;
-          
-            var tpltokenCompile = $compile('<div ng-show=chk class="timeframe ptm prm plm pbm" ><div >'
+
+            var tpltokenCompile = $compile('<div ng-show=chk class="timeframe mtm " ><div class="timeframeinner ptm prm plm pbm" >'
+                            + '<div class="arrow-up"></div><div>'
                              + '<div ng-repeat="Filter in Filters" >'
                              + '<div class="clearfix mtm mbm">'
                              + '<header class="clearfix"><h3 class="pull-left">{{Filter.name}}</h3> <a href="" ng-click="removeContact($index)" class="pull-right">X</a> </header>'
@@ -127,8 +117,6 @@ angular.module('lk.timeframe.tokenFilters', []).directive("lkTimeframefilters", 
                              + '<div class="form-inline"><ul><li ng-hide="Filter.isCollapsed" class="pull-left"> StartDate : '
                              + '<input type="text"  ng-hide="Filter.isCollapsed" ng-model="Filter.startdate" datepicker ></li> <li ng-hide="Filter.isCollapsed"  class="pull-left">EndDate:'
                              + '<input type="text"  ng-hide="Filter.isCollapsed" ng-model="Filter.enddate"  datepicker>'
-                  
-
                              + '</li></ul></div>'
                              + '</div>'
                              + '</div>'
@@ -140,54 +128,28 @@ angular.module('lk.timeframe.tokenFilters', []).directive("lkTimeframefilters", 
                              + '<li ng-repeat="Adddropdown in Adddropdowns">'
                              + '<input type="checkbox" ng-model="Adddropdown.selected" ng-click="addContact(Adddropdown.indexline,Adddropdown.selected,Adddropdown.name)" />'
                              + '<span>{{Adddropdown.name}}</span></li></ul></div></div></div>'
-                             + '<button>Cancel</button><button ng-click="done(Filter)">Done</button></div>'
-                
+                             + '<button>Cancel</button><button ng-click="done(Filter)">Done</button></div></div>'
+
 
 
                       )(scope);
-            //ng-click="showdiv()"
+         
             var tplbuttonCompile = $compile('<button ng-click="showdiv()" >Time frame</button>')(scope);
-           element.append(tplbuttonCompile);
-            //var buttonId, html, title;
-         scope.showdiv = function () {
+            element.append(tplbuttonCompile);
+   
+            scope.showdiv = function () {
 
-             this.chk = !scope.chk;
-             element.append(tpltokenCompile);
+                this.chk = !scope.chk;
+                element.append(tpltokenCompile);
 
-         };
-
-
-
-            buttonId = Math.floor(Math.random() * 10000000000);
-
-            attrs.buttonId = buttonId;
+            };
 
 
 
-            //    element.datepicker({
-            //    dateFormat: 'dd/mm/yy',
-            //    onSelect: function (date) {
-            //        ngModelCtrl.$setViewValue(date);
-            //        scope.$apply();
-            //    }
-            //});  
-            //element.popover({
-            //    content: tpltokenCompile,
-          
-            //    html: true,
-            //    trigger: "manual",
-            //    title: title ,
-            //    placement: "bottom"
-            //});
-      
-    
-            //var chkshow=true;
-            //return element.bind('click', function (e) {
-            //    var dontBubble, pop;
-            //    dontBubble = true;
-            //    e.stopPropagation();
-             
-            //});
+
+
+
+
         }
     };
 });
@@ -199,8 +161,7 @@ module.directive('datepicker', function () {
 
 
     return {
-        //restrict: 'A',
-        //require: 'ngModel',
+
         link: function (scope, element, attrs, ngModelCtrl) {
             $(function () {
                 element.datepicker({
